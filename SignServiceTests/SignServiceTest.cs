24d7a1d9e8ebfa7c1f3a8f87851ad00dbda0f5ac
Alexey Vasilev <valexey.dev@gmail.com>
2018-12-10 19:34:28 +0600

    add: проект для тестов

---
+++ SignServiceTests/SignServiceTest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using SignService;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SignServiceTests
+{
+	[TestFixture]
+	public class SignServiceTest
+	{
+		private const string gost2001 = "8067b09d8564842d4285e400cf91c27c72cf4d0f";
+		private const string gost2012_256 = "af976d0aca919d3df62649501e92145b5ed59967";
+		private const string gost2012_512 = "cbb3d9dca57072feb27ededfea37ce4d3bbffe3f";
+		private const string rsa = "0b386108cd0bc2ff596bb365803b069901a7f5db";
+
+		[Test]
+		public void SignDataTest()
+		{
+			SignServiceProvider provider = new SignServiceProvider(new LoggerFactory());
+			var data = Utils.GetStreamFromFile("Assets\\SignDataTest.txt");
+
+			var sign = provider.Sign(data, gost2001);
+
+			Assert.IsNotNull(sign);
+			Assert.IsTrue(sign.Length > 0);
+
+			X509Certificate2 cert = null;
+
+			bool isVerify = provider.VerifyDetachedMessage(sign, data, false, ref cert);
+
+			Assert.IsTrue(isVerify);
+			Assert.IsNotNull(cert);
+		}
+
+		[Test]
+		public void SignDataAllGostsTest()
+		{
+			SignServiceProvider provider = new SignServiceProvider(new LoggerFactory());
+			var data = Utils.GetStreamFromFile("Assets\\SignDataTest.txt");
+
+			string[] gostCertsThumbprint = new string[] { gost2001, gost2012_256, gost2012_512, rsa };
+
+			for (int i = 0; i < gostCertsThumbprint.Length; i++)
+			{
+				var sign = provider.Sign(data, gostCertsThumbprint[i]);
+
+				Assert.IsNotNull(sign);
+				Assert.IsTrue(sign.Length > 0);
+
+				X509Certificate2 cert = null;
+
+				bool isVerify = provider.VerifyDetachedMessage(sign, data, false, ref cert);
+
+				Assert.IsTrue(isVerify);
+				Assert.IsNotNull(cert);
+			}
+		}
+	}
+}
---
+++ SignServiceTests/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace SignServiceTests
+{
+	internal static class Utils
+	{
+		public static byte[] GetStreamFromFile(string fileName)
+		{
+			var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			var input = File.ReadAllBytes(Path.Combine(path, fileName));
+
+			return input;
+		}
+
+		public static string GetTextFromFile(string fileName)
+		{
+			var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			var text = File.ReadAllText(Path.Combine(path, fileName));
+
+			return text;
+		}
+	}
+}
