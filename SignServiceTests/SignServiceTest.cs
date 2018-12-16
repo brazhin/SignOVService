047e4f0a9c64fc311cbdf1571fc8121a9bf14018
Alexey Vasilev <valexey.dev@gmail.com>
2018-12-16 20:14:47 +0600

    upd: добавил тесты для МР244 и МР255

--- SignServiceTests/SignServiceTest.cs
+++ SignServiceTests/SignServiceTest.cs
@@ -14,29 +14,10 @@ namespace SignServiceTests
 		private const string rsa = "0b386108cd0bc2ff596bb365803b069901a7f5db"; //не используется
 
 		[Test]
-		public void SignDataTest()
-		{
-			SignServiceProvider provider = new SignServiceProvider(new LoggerFactory());
-			var data = Utils.GetStreamFromFile("Assets\\SignDataTest.txt");
-
-			var sign = provider.Sign(data, gost2001);
-
-			Assert.IsNotNull(sign);
-			Assert.IsTrue(sign.Length > 0);
-
-			X509Certificate2 cert = null;
-
-			bool isVerify = provider.VerifyDetachedMessage(sign, data, false, ref cert);
-
-			Assert.IsTrue(isVerify);
-			Assert.IsNotNull(cert);
-		}
-
-		[Test]
 		public void SignDataAllGostsTest()
 		{
 			SignServiceProvider provider = new SignServiceProvider(new LoggerFactory());
-			var data = Utils.GetStreamFromFile("Assets\\SignDataTest.txt");
+			var data = Utils.GetStreamFromFile("Assets\\Smev3\\SignDataTest.txt");
 
 			string[] gostCertsThumbprint = new string[] { gost2001, gost2012_256, gost2012_512, rsa };
 
@@ -57,40 +38,62 @@ namespace SignServiceTests
 		}
 
 		[Test]
-		public void SignXmlTest()
+		public void SignXmlMr300GOST2001()
 		{
-			SignServiceProvider provider = new SignServiceProvider(new LoggerFactory());
-			var files = Utils.GetFilesList("DataWithoutSignature");
-
-			foreach (var file in files)
-			{
-				var data = Utils.GetTextFromFile(file);
-				var signedXml = provider.SignXml(data, Mr.MR300, gost2012_256);
-			}
-
-			foreach (var file in files)
-			{
-				var data = Utils.GetTextFromFile(file);
-				var signedXml = provider.SignXml(data, Mr.MR300, gost2001);
-			}
+			ExecuteOnDataList("Smev3\\DataWithoutSignature", Mr.MR300, gost2001);
 		}
 
 		[Test]
-		public void SignXmlWithSignatureTest()
+		public void SignXmlMr300GOST2012()
+		{
+			ExecuteOnDataList("Smev3\\DataWithoutSignature", Mr.MR300, gost2012_256);
+		}
+
+		[Test]
+		public void SignXmlMr300GOST2001ReplaceSign()
+		{
+			ExecuteOnDataList("Smev3\\DataWithSignature", Mr.MR300, gost2001);
+		}
+
+		[Test]
+		public void SignXmlMr300GOST2012ReplaceSign()
+		{
+			ExecuteOnDataList("Smev3\\DataWithSignature", Mr.MR300, gost2012_256);
+		}
+
+		[Test]
+		public void SignXmlMr244GOST2001()
+		{
+			ExecuteOnDataList("Smev2\\Mr244", Mr.MR244, gost2001);
+		}
+
+		[Test]
+		public void SignXmlMr244GOST2012()
+		{
+			ExecuteOnDataList("Smev2\\Mr244", Mr.MR244, gost2012_256);
+		}
+
+		[Test]
+		public void SignXmlMr255GOST2001()
+		{
+			ExecuteOnDataList("Smev2\\Mr255", Mr.MR255, gost2001);
+		}
+
+		[Test]
+		public void SignXmlMr255GOST2012()
+		{
+			ExecuteOnDataList("Smev2\\Mr255", Mr.MR255, gost2012_256);
+		}
+
+		private void ExecuteOnDataList(string directory, Mr mr, string thumbprint)
 		{
 			SignServiceProvider provider = new SignServiceProvider(new LoggerFactory());
-			var files = Utils.GetFilesList("DataWithSignature");
+			var files = Utils.GetFilesList(directory);
 
 			foreach (var file in files)
 			{
 				var data = Utils.GetTextFromFile(file);
-				var signedXml = provider.SignXml(data, Mr.MR300, gost2012_256);
-			}
-
-			foreach (var file in files)
-			{
-				var data = Utils.GetTextFromFile(file);
-				var signedXml = provider.SignXml(data, Mr.MR300, gost2001);
+				var signedXml = provider.SignXml(data, mr, thumbprint);
 			}
 		}
 	}
