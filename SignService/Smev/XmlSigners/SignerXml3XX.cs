cc708197bf0cac468750cb9bcfa849c94ff845cd
Alexey Vasilev <valexey.dev@gmail.com>
2018-12-16 20:21:14 +0600

    upd: использование SmevXmlHelper в SignerXml3XX

--- SignService/Smev/XmlSigners/SignerXml3XX.cs
+++ SignService/Smev/XmlSigners/SignerXml3XX.cs
@@ -35,15 +35,12 @@ namespace SignService.Smev.XmlSigners
 		private string tagForRequest = string.Empty;
 
 		private int idCounter = 1;
-
-		internal SignedTag ElemForSign { get; set; } = SignedTag.Body;
-		internal bool SignWithId { get; set; } = true;
 		internal Mr MrVersion { get; } = Mr.MR300;
 
-		SignedTag ISignerXml.ElemForSign { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-		bool ISignerXml.SignWithId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public SignedTag ElemForSign { get; set; } = SignedTag.Body;
+		public bool SignWithId { get; set; } = true;
 
-		public SignerXml3XX(ILoggerFactory loggerFactory)
+		internal SignerXml3XX(ILoggerFactory loggerFactory)
 		{
 			this.log = loggerFactory.CreateLogger<SignerXml3XX>();
 		}
@@ -83,9 +80,12 @@ namespace SignService.Smev.XmlSigners
 
 				log.LogDebug("Пытаемся удалить информацию о подписи, если есть.");
 				RemoveCallerInformationSystemSignature(doc.DocumentElement);
-				SetElemId(doc, tagForSign, tagForSignNamespaceUri, SmevMr3xxTags.InformationSystemSignatureId);
+				SmevXmlHelper.SetElemId(doc, tagForSign, tagForSignNamespaceUri, SignWithId, MrVersion, ref idCounter, SmevMr3xxTags.InformationSystemSignatureId);
 
-				signedXml = (Smev3xxSignedXml)AddReference(doc, signedXml, certificate);
+				signedXml = (Smev3xxSignedXml)SmevXmlHelper.AddReference(doc, signedXml, certificate, 
+					SignWithId, MrVersion, ElemForSign, ref idCounter, tagForSign, tagForSignNamespaceUri
+				);
+
 				signedXml.SignedInfo.CanonicalizationMethod = SignedXml.XmlDsigExcC14NTransformUrl;
 
 				log.LogDebug($"Пытаемся получить значение SignatureMethod.");
@@ -355,146 +355,65 @@ namespace SignService.Smev.XmlSigners
 		/// <param name="customNamespace"></param>
 		/// <param name="precedingSibling"></param>
 		/// <returns></returns>
-		private SignedXml AddReference(XmlDocument doc, SignedXml signedXml, IntPtr certificate, string customTag = "", string customNamespace = "", bool precedingSibling = false)
-		{
-			Reference reference = new Reference();
-			string id = string.Empty;
+		//private SignedXml AddReference(XmlDocument doc, SignedXml signedXml, IntPtr certificate, string customTag = "", string customNamespace = "", bool precedingSibling = false)
+		//{
+		//	Reference reference = new Reference();
+		//	string id = string.Empty;
 
-			if (precedingSibling == false)
-			{
-				if (ElemForSign == SignedTag.CustomTag && string.IsNullOrEmpty(customTag) != true)
-				{
-					id = GetElemId(doc, customTag, customNamespace);
+		//	if (precedingSibling == false)
+		//	{
+		//		if (ElemForSign == SignedTag.CustomTag && string.IsNullOrEmpty(customTag) != true)
+		//		{
+		//			id = SmevXmlHelper.GetElemId(doc, customTag, customNamespace, SignWithId);
 
-					if (string.IsNullOrEmpty(id) && SignWithId)
-					{
-						SetElemId(doc, customTag, NamespaceUri.WSSoap11);
-					}
-				}
-				else
-				{
-					id = GetElemId(doc, tagForSign, tagForSignNamespaceUri);
+		//			if (string.IsNullOrEmpty(id) && SignWithId)
+		//			{
+		//				SmevXmlHelper.SetElemId(doc, customTag, NamespaceUri.WSSoap11, SignWithId, MrVersion, ref idCounter);
+		//			}
+		//		}
+		//		else
+		//		{
+		//			id = SmevXmlHelper.GetElemId(doc, tagForSign, tagForSignNamespaceUri, SignWithId);
 
-					if (string.IsNullOrEmpty(id) && SignWithId)
-					{
-						id = SetElemId(doc, tagForSign, tagForSignNamespaceUri);
-					}
-				}
-			}
+		//			if (string.IsNullOrEmpty(id) && SignWithId)
+		//			{
+		//				id = SmevXmlHelper.SetElemId(doc, tagForSign, tagForSignNamespaceUri, SignWithId, MrVersion, ref idCounter);
+		//			}
+		//		}
+		//	}
 
-			if (SignWithId)
-			{
-				reference.Uri = id;
-			}
-			else
-			{
-				reference.Uri = string.Empty;
-			}
+		//	if (SignWithId)
+		//	{
+		//		reference.Uri = id;
+		//	}
+		//	else
+		//	{
+		//		reference.Uri = string.Empty;
+		//	}
 
-			log.LogDebug($"Пытаемся получить значение DigestMethod.");
-			reference.DigestMethod = SignServiceUtils.GetDigestMethod(SignServiceUtils.GetAlgId(certificate));
-			log.LogDebug($"Значение DigestMethod успешно получено: {reference.DigestMethod}.");
+		//	log.LogDebug($"Пытаемся получить значение DigestMethod.");
+		//	reference.DigestMethod = SignServiceUtils.GetDigestMethod(SignServiceUtils.GetAlgId(certificate));
+		//	log.LogDebug($"Значение DigestMethod успешно получено: {reference.DigestMethod}.");
 
-			if (string.IsNullOrEmpty(customTag) != true && ElemForSign == SignedTag.CustomTag)
-			{
-				XmlDsigEnvelopedSignatureTransform envelop = new XmlDsigEnvelopedSignatureTransform();
-				reference.AddTransform(envelop);
-			}
+		//	if (string.IsNullOrEmpty(customTag) != true && ElemForSign == SignedTag.CustomTag)
+		//	{
+		//		XmlDsigEnvelopedSignatureTransform envelop = new XmlDsigEnvelopedSignatureTransform();
+		//		reference.AddTransform(envelop);
+		//	}
 
-			XmlDsigExcC14NTransform c14 = new XmlDsigExcC14NTransform();
-			reference.AddTransform(c14);
+		//	XmlDsigExcC14NTransform c14 = new XmlDsigExcC14NTransform();
+		//	reference.AddTransform(c14);
 
-			SmevTransformAlg smevTransform = new SmevTransformAlg();
-			reference.AddTransform(smevTransform);
+		//	if(MrVersion == Mr.MR300)
+		//	{
+		//		SmevTransformAlg smevTransform = new SmevTransformAlg();
+		//		reference.AddTransform(smevTransform);
+		//	}
 
-			signedXml.AddReference(reference);
+		//	signedXml.AddReference(reference);
 
-			return signedXml;
-		}
-
-		/// <summary>
-		/// Устанавливает значение для тэга с идентификатором, влияет на установку <ds:Reference URI=></Reference>
-		/// </summary>
-		/// <param name="doc"></param>
-		/// <param name="elemName"></param>
-		/// <param name="namespaceUri"></param>
-		/// <param name="specId"></param>
-		/// <returns></returns>
-		private string SetElemId(XmlDocument doc, string elemName, string namespaceUri, string specId = "")
-		{
-			string newId = string.Empty;
-			string existId = GetElemId(doc, elemName, namespaceUri);
-
-			if (string.IsNullOrEmpty(existId) && this.SignWithId)
-			{
-				string lowerName = elemName.ToLower();
-				XmlElement targetElem = (XmlElement)doc.GetElementsByTagName(elemName, namespaceUri)[0] ??
-										(XmlElement)doc.GetElementsByTagName(lowerName, namespaceUri)[0];
-
-				if (string.IsNullOrEmpty(specId))
-				{
-					newId = idCounter.ToString(CultureInfo.InvariantCulture);
-					idCounter++;
-				}
-				else
-				{
-					newId = specId;
-				}
-
-				//if (this.MrVersion == Mr.MR300)
-				//{
-					XmlAttribute idAttr = doc.CreateAttribute("Id");
-					idAttr.Value = newId;
-					targetElem.Attributes.Append((XmlAttribute)doc.ImportNode(idAttr, true));
-				//}
-				//else
-				//{
-				//	XmlAttribute idAttr = doc.CreateAttribute("u", "Id", securityNamespace);
-				//	idAttr.Value = newId;
-				//	targetElem.Attributes.Append((XmlAttribute)doc.ImportNode(idAttr, true));
-				//}
-			}
-
-			return newId;
-		}
-
-		/// <summary>
-		/// Метод получает тэг по идентификатору, влияет на установку <ds:Reference URI=></Reference>
-		/// </summary>
-		/// <param name="doc"></param>
-		/// <param name="elemName"></param>
-		/// <param name="namespaceUri"></param>
-		/// <returns></returns>
-		private string GetElemId(XmlDocument doc, string elemName, string namespaceUri)
-		{
-			string id = "";
-			string lowerName = elemName.ToLower();
-
-			if (SignWithId)
-			{
-				XmlElement targetElem = (XmlElement)doc.GetElementsByTagName(elemName, namespaceUri)[0] ??
-										(XmlElement)doc.GetElementsByTagName(lowerName, namespaceUri)[0];
-
-				if (targetElem.HasAttribute("Id"))
-				{
-					id += "#" + targetElem.GetAttribute("Id");
-				}
-				else if (targetElem.HasAttribute("Id", securityNamespace))
-				{
-					id += "#" + targetElem.GetAttribute("Id", securityNamespace);
-				}
-				else if (targetElem.HasAttribute("id", securityNamespace))
-				{
-					id += "#" + targetElem.GetAttribute("id", securityNamespace);
-				}
-			}
-			else
-			{
-				id = "";
-			}
-
-			return id;
-		}
+		//	return signedXml;
+		//}
 
 		/// <summary>
 		/// Метод поиска тэга в который необходимо добавить подпись
@@ -566,23 +485,7 @@ namespace SignService.Smev.XmlSigners
 			// Получаем тэги Security.
 			XmlNodeList nodeList = node.GetElementsByTagName(SignatureTags.CallerInformationSystemSignatureTag,
 				SignatureTags.CallerInformationSystemSignatureNamespace);
-			RemoveNodes(node, nodeList);
-		}
-
-		/// <summary>
-		/// Удаление списка элементов.
-		/// </summary>
-		/// <param name="node">Откуда удалить.</param>
-		/// <param name="nodeList">Список элементов, которые надо удалить.</param>
-		private void RemoveNodes(XmlElement node, XmlNodeList nodeList)
-		{
-			if (nodeList != null && nodeList.Count > 0)
-			{
-				for (int i = nodeList.Count - 1; i >= 0; i--)
-				{
-					nodeList[i].ParentNode.RemoveChild(nodeList[i]);
-				}
-			}
+			SmevXmlHelper.RemoveNodes(node, nodeList);
 		}
 
 		/// <summary>
